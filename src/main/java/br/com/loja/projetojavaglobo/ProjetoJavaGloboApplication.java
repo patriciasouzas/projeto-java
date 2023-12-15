e6e58b0e13e5b781b42a97993e255499fb0865a8
patriciasouzas <patriciasouzaa89@gmail.com>
2023-12-14 21:51:51 -0300

    Initial commit

---
+++ src/main/java/br/com/loja/projetojavaglobo/ProjetoJavaGloboApplication.java
@@ -0,0 +1,13 @@
+package br.com.loja.projetojavaglobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoJavaGloboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoJavaGloboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/loja/projetojavaglobo/ProjetoJavaGloboApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.loja.projetojavaglobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaGloboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
