417e1e3918b883afb66dabdfe61c165961fc1568
patriciasouzas <patriciasouzaa89@gmail.com>
2023-12-16 21:56:18 -0300

    adicionada alterações em controller

--- src/main/java/br/com/loja/projetojavaglobo/controller/ProdutoController.java
+++ src/main/java/br/com/loja/projetojavaglobo/controller/ProdutoController.java
@@ -1,20 +1,27 @@
 package br.com.loja.projetojavaglobo.controller;
 
+import br.com.loja.projetojavaglobo.exceptions.ProductNotFoundException;
 import br.com.loja.projetojavaglobo.model.dto.RequestProdutoDto;
 import br.com.loja.projetojavaglobo.model.dto.ResponseProdutoDto;
 import br.com.loja.projetojavaglobo.model.entities.Produto;
 import br.com.loja.projetojavaglobo.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping(value = "/produto")
+@RequestMapping(value = "/api/produtos")
 public class ProdutoController {
     private final ProdutoService produtoService;
+    @Autowired
+    public ProdutoController(ProdutoService produtoService){
+        this.produtoService = produtoService;
+    }
+
     @PostMapping("/cadastrar")
     public ResponseProdutoDto createProduto(@RequestBody RequestProdutoDto requestProdutoDto){
         Produto produto = produtoService.createProduto(requestProdutoDto);
@@ -22,13 +29,29 @@ public class ProdutoController {
     }
 
     @GetMapping
-    public List<Produto> findProdutos() throws Exception {
-        return produtoService.findProdutos();
+    public ResponseEntity<List<Produto>> findProdutos() {
+        List<Produto> produtos = produtoService.findProdutos();
+        return ResponseEntity.ok(produtos);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<ResponseProdutoDto> findProdutoById(Long id) throws Exception {
-        Produto produto = produtoService.findById(id);
-        return ResponseEntity.ok(produto.toProdutoDto());
+    public ResponseEntity<ResponseProdutoDto> findProdutoById(@PathVariable Long id) {
+        try {
+            Produto produto = produtoService.findById(id);
+            return ResponseEntity.ok(produto.toProdutoDto());
+        } catch (ProductNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        try {
+            produtoService.deleteProduto(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch (ProductNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
