b8c146af168b045315a163df65b35d6df8bf1245
patriciasouzas <patriciasouzaa89@gmail.com>
2023-12-16 21:58:01 -0300

    adicionada alterações no repository

--- src/main/java/br/com/loja/projetojavaglobo/repository/ProdutoRepository.java
+++ src/main/java/br/com/loja/projetojavaglobo/repository/ProdutoRepository.java
@@ -4,9 +4,5 @@ import br.com.loja.projetojavaglobo.model.entities.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    Optional<Produto> findById(Long id);
-}
+public interface ProdutoRepository extends JpaRepository<Produto, Long> { }
